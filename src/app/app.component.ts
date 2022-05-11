cac334cd3e8fb0c207e2eab8080ab15bf3220942
nilidivija <dkrsb@outlook.com>
2022-05-11 17:28:56 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isBordered: boolean;
+  classesObj: Object;
+  classList: string[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.isBordered = true;
+    this.classList = ['blue', 'round'];
+    this.toggleBorder();
+  }
+
+  toggleBorder(): void {
+    this.isBordered = !this.isBordered;
+    this.classesObj = {
+      bordered: this.isBordered,
+    };
+  }
+
+  toggleClass(cssClass: string): void {
+    const pos: number = this.classList.indexOf(cssClass);
+    if (pos > -1) {
+      this.classList.splice(pos, 1);
+    } else {
+      this.classList.push(cssClass);
+    }
+  }
+}
